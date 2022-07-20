7fa21635f1a89d2822b561c1118cf5f019ce9fce
Lucas <lucas201273@gmail.com>
2022-07-19 21:10:46 -0300

    feat: improvi usability in toggle theme

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -7,12 +7,15 @@ import { ThemeContext } from '../../context/theme/context'
 import * as S from './styles'
 
 const Toggle = () => {
-  const { onChangeTheme } = useContext(ThemeContext)
+  const { onChangeTheme, theme } = useContext(ThemeContext)
 
   return (
     <S.Container>
       <IoSunny size={20} color='#ede42f' />
-      <Checkbox toggle onChange={onChangeTheme} />
+      <Checkbox
+        toggle
+        checked={theme === 'light' ? false : true}
+        onChange={onChangeTheme} />
       <IoMoonOutline size={20} color='#ccc' />
     </S.Container>
   )
